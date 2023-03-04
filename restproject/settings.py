2ff6279022edc7b146c34f0599d1dd3c9a71b852
GusEngers <gustavoengers6441@gmail.com>
2023-03-04 13:21:41 -0300

    Update settings.py

--- restproject/settings.py
+++ restproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-09&-_#qpz35luwx$f84^6%efz1d4m#=s(_c(2^o!tjmvlf=zou'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
