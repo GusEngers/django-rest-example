af9a4856fa160a4c7cccb34635e5376a46febe9d
GusEngers <gustavoengers6441@gmail.com>
2023-03-09 12:12:44 -0300

    Añadido solicitud PUT - fixes
    · Añadido método para solicitud PUT
    · Añadido validaciones para que sea posible enviar solo los campos necesarios
    · Cambiado la key "data" por "error" cuando la solicitud envia un error

--- app/books/views.py
+++ app/books/views.py
@@ -16,18 +16,14 @@ class PostAndList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({"data": f"Libro {serializer.data['title']} creado con éxito"}, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return Response({"error": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
     
     def get(self, request):
         books = Books.objects.all()
         serializer = BookSerializer(books, many=True)
-        data = {
-            "data": []
-        }
         if books:
-            data["data"] = serializer.data
-            return Response(data, status=status.HTTP_200_OK)
-        return Response(data, status=status.HTTP_404_NOT_FOUND)
+            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"error": "La lista de libros está vacia"}, status=status.HTTP_404_NOT_FOUND)
 
 class OneDetail(APIView):
 
@@ -40,6 +36,25 @@ class OneDetail(APIView):
     def get(self, request, pk):
         book = self.get_one(pk)
         if type(book) == str:
-            return Response({"data": book}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": book}, status=status.HTTP_404_NOT_FOUND)
         serializer = BookSerializer(book)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        book = self.get_one(pk)
+        if type(book) == str:
+            return Response({"error": book}, status=status.HTTP_404_NOT_FOUND)
+
+        rd = request.data
+        updates = {
+            "title": rd["title"] if "title" in rd else getattr(book, "title"),
+            "genre": rd["genre"] if "genre" in rd else getattr(book, "genre"),
+            "year": rd["year"] if "year" in rd else getattr(book, "year"),
+            "author": rd["author"] if "author" in rd else getattr(book, "author")
+        }
+        serializer = BookSerializer(book, data=updates)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": f"Libro {serializer.data['title']} actualizado", "upd": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"error": serializer.errors})
+        
